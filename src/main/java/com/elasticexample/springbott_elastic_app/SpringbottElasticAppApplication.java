76f31780e0f0cc6bb33e1afe7943920b2e32fd8d
Reinhard Hütter <meister@MacBook-Air-von-Reinhard-2.local>
2026-03-04 16:11:12 +0100

    commit all changes

---
+++ src/main/java/com/elasticexample/springbott_elastic_app/SpringbottElasticAppApplication.java
@@ -0,0 +1,15 @@
+package com.elasticexample.springbott_elastic_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbottElasticAppApplication {
+
+	public static void main(String[] args) {
+
+        System.out.println("application started");
+        SpringApplication.run(SpringbottElasticAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elasticexample/springbott_elastic_app/SpringbottElasticAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elasticexample.springbott_elastic_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbottElasticAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
